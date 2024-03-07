7b69997fc4b5bfbd42941025aa029eb982f3a947
elmersson <elmerssonrasmus@gmail.com>
2024-03-07 17:33:26 +0100

    feat: layout (#3)

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <p>Hello world!</p>
-      <UserButton />
     </>
   );
 }
