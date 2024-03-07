5154544d725e36475fed04c53deb85f769f79d7a
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-03-07 19:45:26 +0100

    feat: add sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { List } from "./list";
+import { NewButton } from "./new-button";
+
 export const Sidebar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      <div>Sidebar</div>
+      <List />
+      <NewButton />
     </aside>
   );
 };
