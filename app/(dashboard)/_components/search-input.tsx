f7088e4a29cac8ccfa13590213641f640afd98ee
elmersson <elmerssonrasmus@gmail.com>
2024-03-08 19:45:13 +0100

    feat: empty states (#7)

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -2,7 +2,7 @@
 
 import qs from "query-string";
 import { Search } from "lucide-react";
-import { useDebounce } from "usehooks-ts";
+import { useDebounceValue } from "usehooks-ts";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, useEffect, useState } from "react";
 
@@ -11,7 +11,7 @@ import { Input } from "@/components/ui/input";
 export const SearchInput = () => {
   const router = useRouter();
   const [value, setValue] = useState("");
-  const debouncedValue = useDebounce(value, 500);
+  const [debouncedValue] = useDebounceValue(value, 500);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
