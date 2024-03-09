d201809f1bd419decba381e61dcff6fdad76c470
elmersson <elmerssonrasmus@gmail.com>
2024-03-09 12:55:00 +0100

    feat: board cards (#9)

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useQuery } from "convex/react";
 import { EmptyBoards } from "./empty-boards";
 import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
+import { api } from "@/convex/_generated/api";
+import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -13,7 +17,22 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = [];
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined) {
+    return (
+      <div>
+        <h2 className="text-3xl">{query.favorites ? "Favorite boards" : "Team boards"}</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
+  }
 
   if (!data.length && query.search) {
     return <EmptySearch />;
@@ -26,5 +45,24 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   if (!data.length) {
     return <EmptyBoards />;
   }
-  return <div>{orgId}</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">{query.favorites ? "Favorite boards" : "Team boards"}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            id={board._id}
+            title={board.title}
+            authorName={board.authorName}
+            authorId={board.authorId}
+            createdAt={board._creationTime}
+            imageUrl={board.imageUrl}
+            orgId={board.orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
