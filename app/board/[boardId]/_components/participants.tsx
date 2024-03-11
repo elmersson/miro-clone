d4149e87e0e959217a5bf9d04a5ac9779385a15b
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-03-11 17:20:29 +0100

    feat: board page

---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const Participants = () => {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+      <div className="flex gap-x-2">Participants</div>
+    </div>
+  );
+};
