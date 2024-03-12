0a2d20ae43af6b0603ee7f7a8529494c1ed71b52
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-03-12 18:17:57 +0100

    feat: setup liveblocks

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -9,3 +9,8 @@ export const Info = ({ boardId }: InfoProps) => {
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">{boardId}</div>
   );
 };
+export const InfoSkeleton = () => {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
+  );
+};
--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -7,3 +7,7 @@ export const Participants = () => {
     </div>
   );
 };
+
+export const ParticipantsSkeleton = () => {
+  return <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />;
+};
