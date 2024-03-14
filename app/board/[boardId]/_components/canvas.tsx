6f4032a8f8cc36bbc7fcb184a9d7dd1ce6530a04
elmersson <elmerssonrasmus@gmail.com>
2024-03-14 06:25:42 +0100

    feat: room info (#15)

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,18 +1,15 @@
 "use client";
 
+import { Id } from "@/convex/_generated/dataModel";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 
-import { useSelf } from "@/liveblocks.config";
-
 interface CanvasProps {
-  boardId: string;
+  boardId: Id<"boards">;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-  const info = useSelf((me) => me.info);
-
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
