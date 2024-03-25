d04334bc50c7945372d81945017c0b05cd6fb63b
elmersson <elmerssonrasmus@gmail.com>
2024-03-25 17:47:19 +0100

    OHD-316 (#27)

--- app/board/[boardId]/_components/ellipse.tsx
+++ app/board/[boardId]/_components/ellipse.tsx
@@ -1,3 +1,4 @@
+import { colorToCss } from "@/lib/utils";
 import { EllipseLayer } from "@/types/canvas";
 
 interface EllipseProps {
@@ -8,5 +9,23 @@ interface EllipseProps {
 }
 
 export const Ellipse = ({ id, layer, onPointerDown, selectionColor }: EllipseProps) => {
-  return <div>Ellipse</div>;
+  return (
+    <ellipse
+      className="drop-shadow-md"
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        transform: `translate(
+          ${layer.x}px,
+          ${layer.y}px
+        )`,
+      }}
+      cx={layer.width / 2}
+      cy={layer.height / 2}
+      rx={layer.width / 2}
+      ry={layer.height / 2}
+      fill={layer.fill ? colorToCss(layer.fill) : "#000"}
+      stroke={selectionColor || "transparent"}
+      strokeWidth="1"
+    />
+  );
 };
